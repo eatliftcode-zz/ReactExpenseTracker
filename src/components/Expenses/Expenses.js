79a7ca58dad275e0c2cf0c520f774b0f7d1029aa
Mohit <mohit.gpta28@Gmail.com>
2022-01-10 21:25:19 -0600

    added filter for expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,19 +1,27 @@
 import ExpenseItem from "./ExpenseItem";
-
+import ExpensesFilter from "./ExpenseFilter";
 import "./Expenses.css";
 function Expenses(props) {
+  const filterChangeHandler = (year) =>{
+    console.log(year);
+  }
+
   return (
-    <div className="expenses">
-      {props.expenses.map(expense => {
-           return (
-            <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-          )
-      })}
+    <div>
+      <div className="expenses">
+      <ExpensesFilter changeHandler={filterChangeHandler} />
+        {props.expenses.map(expense => {
+              return (
+              <ExpenseItem
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+            )
+        })}
+      </div>
     </div>
+
   );
 }
 export default Expenses;
